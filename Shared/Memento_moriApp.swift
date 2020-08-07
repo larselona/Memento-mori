9c3f9fec4aadddcb8eef7594803a3da3c7a3e7f7
Lars Isdahl <lars.isdahl@gmail.com>
2020-08-07 18:23:50 +0200

    Initial Commit

---
+++ Shared/Memento_moriApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memento_moriApp.swift
+//  Shared
+//
+//  Created by lars on 07/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct Memento_moriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
